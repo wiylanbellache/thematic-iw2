5421f14c302350ac11ea0a116030e853a0242681
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-03-24 12:12:46 +0100

    installing grpc

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'task',
+        protoPath: join(__dirname, 'proto/task.proto'),
+      },
+    },
+  );
 }
 bootstrap();
